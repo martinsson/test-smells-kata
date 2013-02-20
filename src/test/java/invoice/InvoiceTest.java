295d3ee5bf6e0b0d8a02a0911a122497070a84e8
Johan Martinsson <jmartinsson.ext@orange.com>
2013-02-20 17:43:14 +0100

    added some classes

---
+++ src/test/java/invoice/InvoiceTest.java
@@ -0,0 +1,23 @@
+package invoice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InvoiceTest {
+
+	@Test
+	public void testAddItemQuantity_severalQuantity() {
+		Address billingAddress = null;
+		Address shippingAddress = null;
+		Customer customer = null;
+		Product product = null;
+		Invoice invoice = null;
+		try {
+			
+		} finally {
+			// TODO: handle exception
+		}
+	}
+
+}
