be4368691d88e06b17ef4dcb1ef225ca0463adca
Johan Martinsson <martinsson.johan@gmail.com>
2013-02-21 16:51:41 +0100

    example

--- src/test/java/invoice/InvoiceTest.java
+++ src/test/java/invoice/InvoiceTest.java
@@ -1,55 +1,85 @@
 package invoice;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import org.junit.After;
 import org.junit.Test;
 
 public class InvoiceTest {
 
-	@Test
+	private List<Object> objectsToClean = new ArrayList<Object>();;
+
+    @Test
 	public void testAddItemQuantity_severalQuantity() {
-		Address billingAddress = null;
-		Address shippingAddress = null;
-		Customer customer = null;
-		Product product = null;
-		Invoice invoice = null;
-		try {
-			// Set up fixture
-			billingAddress = new Address("12 rue Robert Dumont", "Grenoble", "Isère", "38000", "France");
-			shippingAddress = new Address("12 rue Robert Dumont", "Grenoble", "Isère", "38000", "France");
-			customer = new Customer(99, "Auguste", "Blanc", new BigDecimal("30"), billingAddress, shippingAddress);
-			product = new Product(88, "TV", new BigDecimal("19.99"));
-			invoice = new Invoice(customer);
-			
-			//Exercise SUT
-			invoice.addItemQuantity(product, 5);
-			// Verify outcome
-			List<LineItem> lineItems = invoice.getLineItems();
-			if (lineItems.size() == 1) {
-				LineItem actItem = lineItems.get(0);
-				assertEquals("inv",  invoice, actItem.getInv());
-				assertEquals("prod", product, actItem.getProd());
-				assertEquals("quant", 5, actItem.getQuantity());
-				assertEquals("discount", new BigDecimal("30"), actItem.getPercentDiscount());
-				assertEquals("unit price", new BigDecimal("19.99"), actItem.getUnitPrice());
-				assertEquals("extended price", new BigDecimal("69.96"), actItem.getExtendedPrice());
-			} else {
-				assertTrue("Invoice should have 1 item", false);
-			}
-			
-		} finally {
-			// Teardown
-			deleteObject(invoice);
-			deleteObject(product);
-			deleteObject(customer);
-			deleteObject(billingAddress);
-			deleteObject(shippingAddress);
-		}
+        BigDecimal discountRate = new BigDecimal("30");
+        BigDecimal price = new BigDecimal("19.99");
+
+        Product product = aProductThatCosts(price);
+        Invoice invoice = anInvoiceWith(aCustomerWithDiscountRate(discountRate));
+                
+        
+        //Exercise SUT
+        int quantity = 5;
+        invoice.addItemQuantity(product, quantity);
+        
+        // Verify outcome
+        List<LineItem> lineItems = invoice.getLineItems();
+        assertEquals(1, lineItems.size());
+        LineItem actItem = lineItems.get(0);
+        assertEquals("inv",  invoice, actItem.getInv());
+        BigDecimal expectedTotal = new BigDecimal("69.96");
+        assertEquals("extended price", expectedTotal, actItem.getExtendedPrice());
+
+        //        assertEquals(expectedLineItem, actItem);
 	}
+
+    private Invoice anInvoiceWith(Customer aCustomerWithDiscountRate) {
+        return new Invoice(aCustomerWithDiscountRate);
+    }
+
+    private Product aProductThatCosts(BigDecimal unitPrice) {
+        return new Product(88, "TV", unitPrice);
+    }
+
+    private Customer aCustomerWithDiscountRate(BigDecimal discountRate) {
+        Address billingAddress = anAddress();
+        Address shippingAddress = anAddress();
+        // Set up fixture
+        Customer customer = new Customer(99, "Auguste", "Blanc", discountRate, billingAddress, shippingAddress);
+        return customer;
+    }
+
+    
+     
+    @After
+    public void initBeforeTest() throws Exception {
+        for (Object object : objectsToClean) {
+            try {
+                deleteObject(object);
+            } catch (Throwable e) {
+                // do something to make the test fail after cleaning up all objects (@Rule ExternalRessource)
+            }
+        }
+        
+    }
+    private Address anAddress() {
+        Address address = new Address("12 rue Robert Dumont", "Grenoble", "Isï¿½re", "38000", "France");
+        objectsToClean.add(address);
+        return address;
+    }
+
+    private Address registerObject() {
+        
+        Address address = new Address("12 rue Robert Dumont", "Grenoble", "Isï¿½re", "38000", "France");
+        objectsToClean.add(address);
+        return address;
+    }
 
 	private void deleteObject(Object object) {
 		// some logic to delete object from database
